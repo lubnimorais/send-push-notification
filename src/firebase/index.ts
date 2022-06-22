71c15322ebf8662837a240250ed8709bf391ffd5
Lubni Morais <lubni.morais@gmail.com>
2022-06-22 15:21:13 -0300

    bug: Not send notification

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,10 +1,25 @@
 import firebase from 'firebase-admin';
 import path from 'path';
+import fs from 'fs';
+
+interface ICredentials {
+  client_email: string;
+  private_key: string;
+  project_id: string;
+
+}
 
 const credentials = path.resolve(__dirname, 'firebase.json');
+const readContent = fs.readFileSync(credentials)
+const credentialJSON = JSON.parse(readContent.toString().replace(/(<([^>]+)>)/gi, '').replace(/\r?\n|\r/g, '')) as ICredentials;
+// console.log("🚀 ~ file: index.ts ~ line 8 ~ credentialJSON", credentialJSON.private_key)
 
 const apiFirebase = firebase.initializeApp({
-  credential: firebase.credential.cert(credentials),
+  credential: firebase.credential.cert({
+    clientEmail: credentialJSON.client_email,
+    privateKey: credentialJSON.private_key,
+    projectId: credentialJSON.project_id
+  }),
 })
 
 export { apiFirebase };
